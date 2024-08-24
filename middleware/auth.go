c1588b29e0d7c57729fcd3d3fb43e8cf13f6c64b
Nuthapon <nuthaponm79@gmail.com>
2024-08-24 22:45:09 +0700

    fix: auth middleware

--- middleware/auth.go
+++ middleware/auth.go
@@ -1,23 +1,27 @@
 package middleware
 
 import (
-	"fmt"
-
 	"github.com/gofiber/fiber/v2"
 	"github.com/golang-jwt/jwt"
 )
 
 func AuthMiddleware(c *fiber.Ctx) error {
 	cookie := c.Cookies("jwt")
+	if cookie == "" {
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "missing or invalid jwt token",
+		})
+	}
 
-	jwtSecretKey := "1234"
+	jwtSecretKey := []byte("1234")
 	_, err := jwt.ParseWithClaims(cookie, &jwt.StandardClaims{}, func(token *jwt.Token) (interface{}, error) {
 		return jwtSecretKey, nil
 	})
 
-	fmt.Println("err======>", err)
 	if err != nil {
-		return c.SendStatus(fiber.StatusUnauthorized)
+		return c.Status(fiber.StatusUnauthorized).JSON(fiber.Map{
+			"message": "invalid or expired token",
+		})
 	}
 
 	return c.Next()
